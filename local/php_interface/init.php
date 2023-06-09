72356689492f2f280a886c06d5969ac288417175
starscy <starscy@rambler.ru>
2023-06-09 17:28:31 +0300

    quick_repair_phpdevorg

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,10 +1,18 @@
 <?php
+if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/vendor/autoload.php")) {
+    require_once $_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/vendor/autoload.php";
+}
+
 if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/include/getIDBlockByCode.php")) {
     require_once $_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/include/getIDBlockByCode.php";
 }
 
 if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/modules/dev.site/include.php")) {
     include $_SERVER["DOCUMENT_ROOT"]."/local/modules/dev.site/include.php";
+}
+
+if (file_exists($_SERVER["DOCUMENT_ROOT"]."/local/modules/phpdevorg.cprop/include.php")) {
+    include $_SERVER["DOCUMENT_ROOT"]."/local/modules/phpdevorg.cprop/include.php";
 }
 
 $eventManager = \Bitrix\Main\EventManager::getInstance();
