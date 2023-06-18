628c9244b27894a9c955cb455b0d698cbbf0425e
starscy <starscy@rambler.ru>
2023-06-18 21:20:44 +0300

    del_comp

--- local/components/myComponents/formOnly.result.new/templates/form_application_only/result_modifier.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
-    if ($arQuestion['CAPTION'] == 'Сообщение') {
-        $arResult["QUESTIONS"]['TEXTAREA'] = $arQuestion;
-    }
-}
--- taxi/.section.php
+++
@@ -1,6 +0,0 @@
-<?
-$sSectionName = "taxi";
-$arDirProperties = Array(
-
-);
-?>
